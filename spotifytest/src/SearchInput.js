603d903d70cf6f15be159e12c57d660d86197eaf
Christoper Millington <c.millington@westernsydney.edu.au>
2018-10-16 14:02:43 +1100

    Search parameters updated

--- spotifytest/src/SearchInput.js
+++ spotifytest/src/SearchInput.js
@@ -9,39 +9,35 @@ export default class SearchInput extends Component {
             token: localStorage.getItem('token')
         };
         this.handleSearch = this.handleSearch.bind(this);
+        this.ResultDisplay = this.ResultDisplay.bind(this);
     }
     handleSearch(e){
         if(e.key === 'Enter'){
             this.setState({query: encodeURIComponent(e.target.value.trim())});
-            fetch('https://api.spotify.com/v1/search?q=album:gold%20artist:abba&type=album', 
+            fetch('https://api.spotify.com/v1/search?q=name:' + this.state.query, 
                 {headers:{'Authorization': 'Bearer ' + this.state.token}})
                 .then(response => response.json())
                 .then(data => console.log(data))
+                this.ResultDisplay()
         }
     } 
     render(){
+        let spotifyQueryReq = [
+            { name: "checkTrack", id: "checkTrack", value: "Track"},
+            { name: "checkArtist", id: "Artist", value: "Artist"},
+            { name: "checkAlbum", id: "Album", value: "Album"},
+            { name: "checkPlaylist", id: "Playlist", value: "Playlist"}, 
+        ];
         return(
-            <div>
-                <input type="text" placeholder="Search" onKeyPress={this.handleSearch}/>   
-                <SearchFunction input={this.state.query} token={this.props.token} />
-                {console.log(this.state.query + ' ' + this.state.token)}
-            </div>
+            <div id="searchParams">
+                <input name="search box" type="text" placeholder="Search Spotify" onKeyPress={this.handleSearch}/>
+                {spotifyQueryReq.map((e) => 
+                    <label>{e.value}<input key={parseInt(e.name)} type="checkbox" name={e.name} id={e.id} value={e.value}/></label>)}
+            </div>    
         );
     }
-}
-export class SearchFunction extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            input: '',
-            token: '',
-            results: []
-        };
-    }
-    render(){
-        return(
-            <div><h5>Search Results</h5>{this.props.input}</div>
-        );
+    ResultDisplay() {
+    // <SearchFunction input={this.state.query} token={this.props.token} />
     }
 }
 ReactDOM.render(<SearchInput />,document.getElementById("search"))
