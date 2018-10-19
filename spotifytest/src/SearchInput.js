049734ff6c53e9210391718730decfe2ff949fe8
Christoper Millington <c.millington@westernsydney.edu.au>
2018-10-19 13:36:55 +1100

    Updated index and developed search() more

--- spotifytest/src/SearchInput.js
+++ spotifytest/src/SearchInput.js
@@ -6,50 +6,58 @@ export default class SearchInput extends Component {
         super(props);
         this.state = {
             query: '',
-            token: localStorage.getItem('token')
+            token: localStorage.getItem('token')            
         };
         this.handleSearch = this.handleSearch.bind(this);
-        this.ResultDisplay = this.ResultDisplay.bind(this);
     }
+
     handleSearch(e){
+        let nodeList = [];
         if(e.key === 'Enter'){
             this.setState({query: encodeURIComponent(e.target.value.trim())});
             fetch('https://api.spotify.com/v1/search?q=name:' + this.state.query, 
                 {headers:{'Authorization': 'Bearer ' + this.state.token}})
                 .then(response => response.json())
                 .then(data => console.log(data))
-                this.ResultDisplay()
+            for (let i = 0; i < SQ.length; i++){
+                if(document.getElementById(SQ[i].id).checked === true){
+                    console.log(SQ[i].value)
+                }
+            }
         }
     }
-    handleCheck(e){
-        console.log(e.name);
-    }
-    ResultDisplay() {
-        // <SearchFunction input={this.state.query} token={this.props.token} />
-    } 
+
     render(){
-        let spotifyQueryReq = [
-            { name: "checkTrack", id: "checkTrack", value: "Track"},
-            { name: "checkArtist", id: "Artist", value: "Artist"},
-            { name: "checkAlbum", id: "Album", value: "Album"},
-            { name: "checkPlaylist", id: "Playlist", value: "Playlist"}, 
-        ];
         return(
-            <div className="container">
-                <div id="searchParams">
-                    <input name="search box" type="text" placeholder="Search Spotify" onKeyPress={this.handleSearch}/>
-                    {spotifyQueryReq.map((e) => 
-                    <div>
-                        <label  key={e.id}>{e.value}
-                        <input  key={parseInt(e.name)} 
-                                type="checkbox" name={e.name} 
-                                id={e.id} value={e.value} 
-                                onChange={console.log(e.value)}/>
-                        </label>
-                    </div>)}
-                </div>
-            </div>
+            <input name="search box" type="text" placeholder="Search Spotify" onKeyPress={this.handleSearch}/>
         );
     }
 }
+
+function ShowList(props){
+    const ListOptions = (
+        <div>
+            {SQ.map((object) => 
+                <li key={object.value}>
+                    <input type="checkbox" key={object.value.toString()} id={object.id} />
+                    {' '+ object.id}
+                </li>
+            )}
+        </div>
+    );
+    return(
+        <div>
+            {ListOptions}
+        </div>
+    );
+}
+
+const SQ = [
+    { id: "Track", value: "track"},
+    { id: "Artist", value: "artist"},
+    { id: "Album", value: "album"},
+    { id: "Playlist", value: "playlist"}, 
+];
+
 ReactDOM.render(<SearchInput />, document.getElementById("search"))
+ReactDOM.render(<ShowList SQ={SQ} />, document.getElementById("searchparams"))
