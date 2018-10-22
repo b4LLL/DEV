3e0871109633a7bd7f76ed49ee00cfae24d193ec
Christoper Millington <c.millington@westernsydney.edu.au>
2018-10-22 12:04:01 +1100

    Search query working now.

--- spotifytest/src/SearchInput.js
+++ spotifytest/src/SearchInput.js
@@ -6,31 +6,39 @@ export default class SearchInput extends Component {
         super(props);
         this.state = {
             query: '',
-            token: localStorage.getItem('token')            
+            token: localStorage.getItem('token')                //needs to be moved to server session
         };
         this.handleSearch = this.handleSearch.bind(this);
     }
 
     handleSearch(e){
+        let checkedCount = 0
+        let typeArray = []
         if(e.key === 'Enter'){
-            this.setState({query: encodeURIComponent(e.target.value.trim())}, () => {
-                console.log(this.state.query)
-                fetch('https://api.spotify.com/v1/search?q=' + this.state.query +'&type=track', 
-                    {headers:{'Authorization': 'Bearer ' + this.state.token}})
-                    .then(response => response.json())
-                    .then(data => console.log(data))
-            })
             for (let i = 0; i < SQ.length; i++){
-                if(document.getElementById(SQ[i].id).checked === true)
+                if(document.getElementById(SQ[i].id).checked === true){
                     console.log(SQ[i].value)
+                    typeArray.push(SQ[i].value)
+                    checkedCount++
+                }
             }
+            if(checkedCount !== 0){
+                this.setState({query: encodeURIComponent(e.target.value.trim())}, () => {
+                    console.log(this.state.query)
+                    console.log(typeArray.toString())
+                    fetch('https://api.spotify.com/v1/search?q=' + this.state.query +'&type=' + typeArray.toString(), 
+                        {headers:{'Authorization': 'Bearer ' + this.state.token}})
+                        .then(response => response.json())
+                        .then(data => console.log(data))
+                })
+            }else alert("Please select which search option")
         }
     }
 
     render(){
         return(
             <div>
-                <iframe src="https://open.spotify.com/embed/track/0nLiqZ6A27jJri2VCalIUs" width="300" height="380" frameBorder ="0" allowtransparency="true" allow="encrypted-media" title="Spotify Player">Spotify Player</iframe>
+                <iframe src="https://open.spotify.com/embed/track/5sICkBXVmaCQk5aISGR3x1" width="300" height="380" frameBorder ="0" allowtransparency="true" allow="encrypted-media" title="Spotify Player">Spotify Player</iframe>
                 <input name="search box" type="text" placeholder="Search Spotify" onKeyPress={this.handleSearch}/>                
             </div>
             
@@ -38,18 +46,17 @@ export default class SearchInput extends Component {
     }
 }
 
-function ShowList(props){
+function ShowOptions(props){
+    let checkVal = false;
     const ListOptions = (
-            SQ.map((object) => (
-                <ListGroupItem key={object.value} className="list-group">
-                    <Label>{object.id} <Checkbox key={object.value.toString()} id={object.id}/></Label>
-                </ListGroupItem>) //replace this with https://react-bootstrap.github.io/components/button-group/#btn-groups-toggle-group-props
-            )
+        SQ.map((object) => (
+            <ListGroupItem key={object.value} className="list-group">
+                <Label>{object.id} <Checkbox key={object.value.toString()} id={object.id} defaultChecked={(object.id === "Track") ? true : checkVal}/></Label>
+            </ListGroupItem>)
+        )
     );
     return(
-        <b>
-            {ListOptions}
-        </b>
+        <span>{ListOptions}</span>
     );
 }
 
@@ -61,4 +68,4 @@ const SQ = [
 ];
 
 ReactDOM.render(<SearchInput />, document.getElementById("search"))
-ReactDOM.render(<ShowList SQ={SQ} />, document.getElementById("searchparams"))
+ReactDOM.render(<ShowOptions SQ={SQ} />, document.getElementById("searchparams"))
